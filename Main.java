55caded2bdd45bffd34d52a440a1f35f42525a2d
FilipRzezicki <126695983+FilipRzezicki@users.noreply.github.com>
2023-05-24 14:32:55 +0000

    Zmienione komentarze

--- Main.java
+++ Main.java
@@ -9,12 +9,12 @@ class Main {
     double a = scanner.nextDouble();
     System.out.println("Podaj wartość b: "); //podajemy wartość b
      double b = scanner.nextDouble();
-     if (a == 0) { //jeśli współczynnik jest równy 0 wtedy nie ma równania liniowego
+     if (a == 0) { //jeśli współczynnik jest równy 0 wtedy nie ma równania liniowego ~ definicja prosto ze strony ministerstwa
             System.out.println("To nie jest równanie liniowe.");
         } else {
             System.out.println("To jest równanie liniowe.");
             System.out.println("Równanie: " + a + "*x + " + b + " = 0"); // tutaj jest wyświetlane równanie
-            double x = -b / a; //jeśli występuje równanie liniowe  to posiada miejsce zerowe
+            double x = -b / a; //jeśli występuje równanie liniowe  to posiada miejsce zerowe ~ definicja prosto ze strony ministerstwa
             System.out.println("Rozwiązanie równania: x = " + x); //wypisywanie rozwiązania równania
         }
 
